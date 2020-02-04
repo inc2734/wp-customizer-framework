f4ef880a0eb409ae246e2b974f326c791f5e5eba
Takashi Kitajima <inc@2inc.org>
2020-02-04 14:00:28 +0900

    Update wpcs

--- bin/phpunit.sh
+++ bin/phpunit.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
-if [ -e /tmp/wordpress-tests-lib ]; then
+TMPDIR=${TMPDIR-/tmp}
+TMPDIR=$(echo $TMPDIR | sed -e "s/\/$//")
+WP_TESTS_DIR=${WP_TESTS_DIR-$TMPDIR/wordpress-tests-lib}
+
+if [ -e ${WP_TESTS_DIR} ] && [ -e ${WP_TESTS_DIR}/includes/functions.php ]; then
 
   themedir=$(pwd)
 
-  cd $themedir;
-  vendor/bin/phpunit
+  cd ${themedir};
+  vendor/bin/phpunit --configuration=${themedir}/.phpunit.xml.dist
   exit 0
 fi
 
 dir=$(cd $(dirname $0) && pwd)
-bash "$dir/wpphpunit.sh"
+bash "${dir}/wpphpunit.sh"
--- bin/wpphpunit.sh
+++ bin/wpphpunit.sh
@@ -2,23 +2,28 @@
 
 set -e;
 
+TMPDIR=${TMPDIR-/tmp}
+TMPDIR=$(echo $TMPDIR | sed -e "s/\/$//")
+WP_TESTS_DIR=${WP_TESTS_DIR-$TMPDIR/wordpress-tests-lib}
+WP_CORE_DIR=${WP_CORE_DIR-$TMPDIR/wordpress/}
+
 themedir=$(pwd)
 
-cd $themedir
+cd ${themedir}
 
-if [ -e $themedir/bin/install-wp-tests.sh ]; then
+if [ -e ${themedir}/bin/install-wp-tests.sh ] || [ ! -e ${WP_TESTS_DIR}/includes/functions.php ]; then
   echo 'DROP DATABASE IF EXISTS wordpress_test;' | mysql -u root
 
-  if [ -e /tmp/wordpress ]; then
-    rm -fr /tmp/wordpress
+  if [ -d ${WP_CORE_DIR} ]; then
+    rm -fr ${WP_CORE_DIR}
   fi
 
-  if [ -e /tmp/wordpress-tests-lib ]; then
-    rm -fr /tmp/wordpress-tests-lib
+  if [ -d ${WP_TESTS_DIR} ]; then
+    rm -fr ${WP_TESTS_DIR}
   fi
 
-  bash "$themedir/bin/install-wp-tests.sh" wordpress_test root '' localhost latest;
-  phpunit
+  bash "${themedir}/bin/install-wp-tests.sh" wordpress_test root '' localhost latest;
+  vendor/bin/phpunit --configuration=${themedir}/.phpunit.xml.dist
 else
-  echo "$themedir/bin/install-wp-tests.sh not found."
+  echo "${themedir}/bin/install-wp-tests.sh not found."
 fi;
